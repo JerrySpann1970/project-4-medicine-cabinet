b44391617ceabc8faf25484091e2cb12385c27f2
jerryspann <jerryspann0@gmail.com>
2025-05-01 10:17:14 -0700

    wip: added the medication model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Medication(models.Model):
+    name = models.CharField(max_length=100)
+    strength = models.CharField(max_length=100)
+    dosage = models.CharField(max_length=50)
+    usage= models.CharField(max_length=50)
+    quantity = models.IntegerField()
+    refills = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Medication
 
 # Define the home view function
 def home(request):
@@ -8,22 +9,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class Medication:
-    def __init__(self, name, strength, dosage, usage, quantity, refills):
-        self.name = name
-        self.strength = strength
-        self.dosage = dosage
-        self.usage = usage
-        self.quantity = quantity
-        self.refills = refills
-
-medications = [
-    Medication('Zoloft', '100MG', 'Take 1 tablet daily', 'For Depression', 90, 3),
-    Medication('Tylonol', '10MG', 'Take 1 tablets every 8 hours', 'For Pain', 30, 3),
-    Medication('Trazodone', '50MG', 'Take 1 tablet at bedtime', 'For Anxiety', 30, 2),
-    Medication('Valsartin', '10Mg', 'Take 1 tablet daily', 'For High Blood Pressure', 90, 0),
-]
-
 def medication_index(request):
-    return render(request, 'medications'
-    '/index.html', {'medications': medications})
+    medications = Medication.objects.all()
+    return render(request, 'medications/index.html', {'medications': medications})
