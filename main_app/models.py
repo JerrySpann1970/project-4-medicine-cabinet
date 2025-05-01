d4dc37de014cd76f14bcd2aca3afb0a0e182de8d
jerryspann <jerryspann0@gmail.com>
2025-05-01 11:52:53 -0700

    added a form route to show new meds

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Medication(models.Model):
@@ -11,3 +12,6 @@ class Medication(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('medication-detail', kwargs={'medication_id': self.id})
